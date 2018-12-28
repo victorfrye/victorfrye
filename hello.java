85a13dfc7c0e6004a90627d71a8bbf222df77067
Victor W. Frye <vwfrye95@outlook.com>
2018-12-28 16:04:39 -0500

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,19 @@
+/**
+ * My first program using Java.
+ * <p>
+ * This is my first program using the java programming language. It is a simple,
+ * hello world program.
+ * </p>
+ * 
+ * @author Victor W. Frye <vwfrye95@outlook.com>
+ * @version 1.0
+ * @since 1.0
+ */
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello, World!"); // outputs 'Hello, World!'
+	}
+}
